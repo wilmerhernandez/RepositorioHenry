fa903582a128724afea1477544b505f79115b641
wilmer <wilmer.hernandez.ext@itau.co>
2023-04-19 14:01:18 -0500

    add File.js

---
+++ CarpetaHenry/Curso/File.js
@@ -0,0 +1,24 @@
+/**
+ * Esta es una variable global
+ * declarada como "var" lo cual actualmente no es recomendado
+ * Una variable var es un tipo de variable que se auto declara.
+ * Aunque esto puede ahorrar trabajo al desarrollador no es recomendado
+ * ya que tu como desarrollador debes asegurar la calidad del codigo y control sobre el.
+ */
+
+var cajon;
+
+
+
+
+/**
+ * Variable tipo let
+ * con este tipo de variable puedes asignarle el tipo de variable una sola vez.
+ * y modificar el dato de la variable infinitas veces
+ */
+let string = 'dev'; //string
+let number = 0; // number
+let bool = false; // boolean
+let array = []; // array
+let object = {} // object
+let undefined = undefined; // undefined
--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 Repositorio Henry
 
 # se incluye carpeta llamada "CarpetaHenry"
-
+En esta carpeta guardare archivos siguiendo el prep course de henry especificando un poco mas conceptualmente para una mayor preparacion.
 
