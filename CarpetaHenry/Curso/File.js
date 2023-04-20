92811d99f106cb1486dcf6ef441f16d3b5031978
wilmer <wilmer.hernandez.ext@itau.co>
2023-04-20 11:23:12 -0500

    operations

--- CarpetaHenry/Curso/File.js
+++ CarpetaHenry/Curso/File.js
@@ -8,17 +8,53 @@
 
 var cajon;
 
-
-
-
 /**
  * Variable tipo let
  * con este tipo de variable puedes asignarle el tipo de variable una sola vez.
  * y modificar el dato de la variable infinitas veces
  */
-let string = 'dev'; //string
-let number = 0; // number
-let bool = false; // boolean
-let array = []; // array
-let object = {} // object
-let undefined = undefined; // undefined
+let string = "dev"; //string cadena de texto
+let number = 0; // number solo numero
+let bool = false; // boolean bandera falso o verdadero
+let array = []; // array lista de datos
+let object = {}; // object conjunto de datos
+//let undefined = undefined;  undefined no definida
+let nul = null; // null definida pero sin data
+
+/**
+ * variable tipo const
+ * es una variable que no se le puede modificar en su todalidad despues de declararla
+ */
+
+const variable = "este no se puede modificar";
+
+// en javascript se puede tambien hacer operaciones como : + , * , - , /
+
+let a = 5,
+  b = 6;
+
+  /**
+   * metodo que recibe como parametro el numero a y numero b mas el tipo de operacion a realizar
+   * @param {*} operationType 
+   * @param {*} a 
+   * @param {*} b 
+   * @returns 
+   */
+const operation = (operationType, a, b) => {
+  const objectOperations = {
+    suma: () => ((a + b)),
+    resta: () => ((a - b)),
+    multiplicacion: () =>(( a * b)),
+    divicion: () => ((a / b)),
+  };
+  return objectOperations[operationType]();
+};
+
+console.log("suma",operation("suma",a,b));
+console.log("resta",operation("resta",a,b));
+console.log("multiplicacion",operation("multiplicacion",a,b));
+console.log("divicion",operation("divicion",a,b));
+
+// cuando uno usa la operacion + con caracteres de texto javascript concatena
+
+console.log("suma",operation("suma","esto no ","suma"));
--- README.md
+++ README.md
@@ -3,4 +3,6 @@ Repositorio Henry
 
 # se incluye carpeta llamada "CarpetaHenry"
 En esta carpeta guardare archivos siguiendo el prep course de henry especificando un poco mas conceptualmente para una mayor preparacion.
+* en el archivo File.js tipos de variables
+* en el archivo File.js operaciones
 
